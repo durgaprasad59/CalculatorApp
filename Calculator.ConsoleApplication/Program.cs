a492e9fa8db1570720fb7a8e3ee94bd75e5cfb1c
Avinash M <avinashm201993@gmail.com>
2018-08-12 09:41:46 -0500

    Addition is implemented

--- Calculator.ConsoleApplication/Program.cs
+++ Calculator.ConsoleApplication/Program.cs
@@ -10,6 +10,23 @@ namespace Calculator.ConsoleApplication
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("*** Calculator App ***");
+            Console.Write("Enter first number: ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            Addition(firstNumber, secondNumber);
+            Console.WriteLine("--------------");
         }
+
+        static void Addition(double firstNumber, double secondNumber)
+        {
+            double sum = firstNumber + secondNumber;
+            Console.WriteLine($"Addition of numbers: { sum }");
+        }
+
+        
     }
+
+    
 }
