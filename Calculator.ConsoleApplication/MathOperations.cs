e8f09819d77b9302524777302052811970c43bec
Avinash M <avinashm201993@gmail.com>
2018-08-15 10:09:34 -0500

    Implemented static MathOperations class

---
+++ Calculator.ConsoleApplication/MathOperations.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.ConsoleApplication
+{
+    // Avinash
+    static class MathOperations
+    {
+        static MathOperations()
+        {
+            Console.WriteLine("--- In Static Class ---");
+        }
+
+        internal static void Addition(double firstNumber, double secondNumber)
+        {
+            if (firstNumber > 0 && secondNumber > 0)
+            {
+                double sum = firstNumber + secondNumber;
+                Console.WriteLine($"Addition of numbers: { sum }");
+            }
+            else
+            {
+                Console.WriteLine("Additon: Numbers should be greater than zero.");
+            }
+        }
+
+        internal static void Subtraction(double firstNumber, double secondNumber)
+        {
+            if (firstNumber != 0 && secondNumber != 0)
+            {
+                double sum = firstNumber - secondNumber;
+                Console.WriteLine($"Subtraction of numbers: { sum }");
+            }
+            else
+            {
+                Console.WriteLine("Subtraction of two zeros is zero");
+            }
+        }
+
+        internal static void Multiplication(double firstNumber, double secondNumber)
+        {
+            decimal Multiplier = (decimal)firstNumber * (decimal)secondNumber;
+
+            Console.WriteLine("Multiplication of Numbers: {0}", Multiplier.ToString());
+        }
+
+        internal static void Division(double firstNumber, double secondNumber)
+        {
+            if (secondNumber != 0)
+            {
+                double quotient = firstNumber / secondNumber;
+                Console.WriteLine($"Division of Numbers: {quotient:0.000}");
+            }
+            else
+            {
+                Console.WriteLine("Division: Denominator cannot be zero.");
+            }
+
+        }
+
+        internal static void Remainder(double firstNumber, double secondNumber)
+        {
+            if (secondNumber != 0)
+            {
+                double remainder = firstNumber % secondNumber;
+                Console.WriteLine($"Remainder of Numbers: {remainder:0.000}");
+            }
+            else
+            {
+                Console.WriteLine("Remainder: Denominator cannot be zero.");
+            }
+
+        }
+    }
+
+    
+    
+}
--- Calculator.ConsoleApplication/Program.cs
+++ Calculator.ConsoleApplication/Program.cs
@@ -12,43 +12,19 @@ namespace Calculator.ConsoleApplication
         {
             Console.WriteLine("*** Calculator App ***");
             Console.Write("Enter first number: ");
-            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            double firstNumber = Convert.ToDouble(Console.ReadLine());            
             Console.Write("Enter second number: ");
             double secondNumber = Convert.ToDouble(Console.ReadLine());
-            Addition(firstNumber, secondNumber);
-            Subtraction(firstNumber, secondNumber);
-            Multiplication(firstNumber, secondNumber);
+
+            MathOperations.Addition(firstNumber, secondNumber);
+            MathOperations.Subtraction(firstNumber, secondNumber);
+            MathOperations.Multiplication(firstNumber, secondNumber);
+            MathOperations.Division(firstNumber, secondNumber);
+
             Console.WriteLine("--------------");
             Console.ReadKey();
         }
 
-        static void Addition(double firstNumber, double secondNumber)
-        {
-            double sum = firstNumber + secondNumber;
-            Console.WriteLine($"Addition of numbers: { sum }");
-        }
-
-        // Durga Subtraction code
-
-        static void Subtraction(double firstNumber, double secondNumber)
-        {
-            double subtract = firstNumber - secondNumber;
-            Console.WriteLine("Subtraction of Numbers: {0}", subtract);                
-        }
-
-        // Durga Multiplication code 
-
-        static void Multiplication(double firstNumber, double secondNumber)
-        {
-            double Multiplier = firstNumber * secondNumber;
-
-            //string result = String.Format("{0:0}", Multiplier);               
-            //Console.WriteLine("Multiplication of Numbers: {0}", result);
-
-            //Console.WriteLine($"Multiplication of Numbers: {Multiplier:0}");
-
-            Console.WriteLine("Multiplication of Numbers: {0}", Multiplier.ToString("0"));
-        }
     }
 
     
